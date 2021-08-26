a582af0b0038ccda5ecd402ac8e5200436ae1771
hosasd <hezzeldin1@gmail.com>
2021-08-26 09:55:11 -0700

    convert callback to rxKotlin

--- app/src/main/java/com/example/myfacebook/data/PostInterface.kt
+++ app/src/main/java/com/example/myfacebook/data/PostInterface.kt
@@ -1,11 +1,13 @@
 package com.example.myfacebook.data
 
 import com.example.myfacebook.models.PostModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface PostInterface {
 
     @GET("posts")
-    fun getPosts(): Call<MutableList<PostModel>>
+    fun getPosts(): Single<MutableList<PostModel>>
 }
--- app/src/main/java/com/example/myfacebook/ui/PostViewModel.kt
+++ app/src/main/java/com/example/myfacebook/ui/PostViewModel.kt
@@ -5,6 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myfacebook.data.PostInterface
 import com.example.myfacebook.models.PostModel
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.schedulers.Schedulers.io
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,7 +21,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class PostViewModel : ViewModel() {
+    private val TAG = "tag"
     var postLD : MutableLiveData<MutableList<PostModel>> = MutableLiveData()
+    val compositeDisposable : CompositeDisposable = CompositeDisposable()
     private val BASE_URL : String = "https://jsonplaceholder.typicode.com/"
     private var postInterface : PostInterface
 
@@ -20,24 +31,36 @@ class PostViewModel : ViewModel() {
         val retrofit : Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
         postInterface = retrofit.create(PostInterface::class.java)
     }
 
     fun getPosts(){
 
-        postInterface.getPosts().enqueue(object :Callback<MutableList<PostModel>>{
-            override fun onResponse(
-                call: Call<MutableList<PostModel>>,
-                response: Response<MutableList<PostModel>>
-            ) {
-                Log.d("TAG", "onResponse: "+ (response.body()?.get(0)?.id).toString())
-                postLD.value = response.body()
+        val observable: Single<MutableList<PostModel>> = postInterface.getPosts()
+            .subscribeOn(io())
+            .observeOn(AndroidSchedulers.mainThread())
+        var singleObserver: SingleObserver<MutableList<PostModel>> = object: SingleObserver<MutableList<PostModel>> {
+            override fun onSubscribe(d: Disposable) {
+                compositeDisposable.add(d)
             }
 
-            override fun onFailure(call: Call<MutableList<PostModel>>, t: Throwable) {
-                Log.d("TAG", "onFailure: ")
+            override fun onSuccess(t: MutableList<PostModel>) {
+                postLD.value = t
             }
-        })
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError: ${e.message}")
+            }
+
+        }
+        observable.subscribe(singleObserver)
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
